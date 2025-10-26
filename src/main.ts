26629bef6a50fc4d4e6e194b9efd781ffe29a9bb
lovekeqing <2911331070@qq.com>
2025-10-26 15:50:45 +0800

    feat: 更新模版

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,17 @@
 export function main() {
-  console.log(Game.getMyShips());
+  const ships = Game.getMyShips();
+  ships.forEach((ship) => {
+    runShip(ship);
+  });
+}
+
+function runShip(ship: Ship) {
+  const galaxy = Game.getGalaxy(ship.galaxyId);
+  const planets = galaxy.getPlanets();
+  const planet = planets[0];
+  if (ship.pos.getRangeTo(planet.pos) > 1) {
+    ship.moveTo(planet.pos);
+  } else {
+    ship.harvest(planet);
+  }
 }
